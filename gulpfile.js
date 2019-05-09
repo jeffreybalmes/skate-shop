085a96863d1d71cffbd1bd99cd5a7057d622e9d7
jeffreybalmes <31235599+jeffreybalmes@users.noreply.github.com>
2019-05-09 13:01:48 +0800

    revised gulp, added dependencies and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,11 @@ var browserify   = require( 'browserify' );
 var source       = require( 'vinyl-source-stream' );
 var buffer       = require( 'vinyl-buffer' );
 var stripDebug   = require( 'gulp-strip-debug' );
+var concat       = require( 'gulp-concat' );
 
 // Utility plugins
+var imagemin     = require( 'gulp-imagemin' );
+var newer        = require( 'gulp-newer' );
 var rename       = require( 'gulp-rename' );
 var sourcemaps   = require( 'gulp-sourcemaps' );
 var notify       = require( 'gulp-notify' );
@@ -25,36 +28,42 @@ var gulpif       = require( 'gulp-if' );
 var browserSync  = require( 'browser-sync' ).create();
 
 // Project related variables
-var styleSRC     = './src/scss/style.scss';
+var projectURL   = 'http://localhost/03-wordpress/wordpress/';
+
+var styleSRC     = './src/sass/*.scss';
 var styleURL     = './dist/css/';
 var mapURL       = './';
 
-var jsSRC        = './src/js/';
-var jsFront      = 'main.js';
-var jsFiles      = [ jsFront ];
+var jsSRC        = './src/js/*.js';
+var jsFiles      = 'app.min.js';
 var jsURL        = './dist/js/';
 
-var imgSRC       = './src/images/**/*';
+var imgSRC       = './src/images/*';
 var imgURL       = './dist/images/';
 
-var fontsSRC     = './src/fonts/**/*';
+var fontsSRC     = './src/fonts/*';
 var fontsURL     = './dist/fonts/';
 
-var htmlSRC     = './src/**/*.html';
-var htmlURL     = './dist/';
+var iconsSRC     = './bower_components/font-awesome/css/font-awesome.min.css';
+var iconsURL     = './dist/css/';
 
-var styleWatch   = './src/scss/**/*.scss';
-var jsWatch      = './src/js/**/*.js';
-var imgWatch     = './src/images/**/*.*';
-var fontsWatch   = './src/fonts/**/*.*';
-var htmlWatch    = './src/**/*.html';
+// var htmlSRC     = './src/**/*.html';
+// var htmlURL     = './dist/';
+
+var phpWatch     = './**/*.php';
+var styleWatch   = './src/sass/**';
+var jsWatch      = './src/js/*.js';
+var imgWatch     = './src/images/originals/*';
+var fontsWatch   = './src/fonts/*.*';
+var iconsWatch   = './bower_components/font-awesome/css/font-awesome.min.css';
+// var htmlWatch    = './src/**/*.html';
 
 // Tasks
 function browser_sync() {
 	browserSync.init({
-		server: {
-			baseDir: './dist/'
-		}
+      proxy: projectURL,
+      browser: 'chrome',
+      injectChanges: true
 	});
 }
 
@@ -64,11 +73,14 @@ function reload(done) {
 }
 
 function css(done) {
-	src( [ styleSRC ] )
+	src( styleSRC )
 		.pipe( sourcemaps.init() )
 		.pipe( sass({
-			errLogToConsole: true,
-			outputStyle: 'compressed'
+         errLogToConsole: true,
+         indentedSyntax: true,
+         outputStyle: 'compressed',
+         noCache: true,
+         lineNumbers: false
 		}) )
 		.on( 'error', console.error.bind( console ) )
 		.pipe( autoprefixer({ browsers: [ 'last 2 versions', '> 5%', 'Firefox ESR' ] }) )
@@ -79,60 +91,60 @@ function css(done) {
 	done();
 };
 
-function js(done) {
-	jsFiles.map( function( entry ) {
-		return browserify({
-			entries: [jsSRC + entry]
-		})
-		.transform( babelify, { presets: [ '@babel/preset-env' ] } )
-		.bundle()
-		.pipe( source( entry ) )
-		.pipe( rename( {
-			extname: '.min.js'
-        } ) )
-		.pipe( buffer() )
-		.pipe( gulpif( options.has( 'production' ), stripDebug() ) )
-		.pipe( sourcemaps.init({ loadMaps: true }) )
-		.pipe( uglify() )
-		.pipe( sourcemaps.write( '.' ) )
-		.pipe( dest( jsURL ) )
-		.pipe( browserSync.stream() );
-	});
-	done();
+function js(){
+   return src( jsSRC )
+      .pipe(concat( jsFiles ))
+      .pipe( sourcemaps.init({ loadMaps: true }) )
+      .pipe( uglify() )
+      .pipe( sourcemaps.write( '.' ) )
+      .pipe( dest( jsURL ) )
+      .pipe(browserSync.stream());
+};
+
+function images() {
+   return src(imgSRC)
+      .pipe(plumber())
+      .pipe(newer(imgURL))
+      .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+      .pipe(dest(imgURL));
 };
 
 function triggerPlumber( src_file, dest_file ) {
 	return src( src_file )
 		.pipe( plumber() )
 		.pipe( dest( dest_file ) );
-}
-
-function images() {
-	return triggerPlumber( imgSRC, imgURL );
 };
 
 function fonts() {
 	return triggerPlumber( fontsSRC, fontsURL );
 };
 
-function html() {
-	return triggerPlumber( htmlSRC, htmlURL );
+function icons() {
+	return triggerPlumber( iconsSRC, iconsURL );
 };
+
+// function html() {
+// 	return triggerPlumber( htmlSRC, htmlURL );
+// };
 
 function watch_files() {
 	watch(styleWatch, series(css, reload));
+   watch(phpWatch, reload);
 	watch(jsWatch, series(js, reload));
 	watch(imgWatch, series(images, reload));
 	watch(fontsWatch, series(fonts, reload));
-	watch(htmlWatch, series(html, reload));
-	src(jsURL + 'main.min.js')
-		.pipe( notify({ message: 'Gulp is Watching, Happy Coding!' }) );
+   watch(iconsWatch, series(icons, reload));
+
+	// watch(htmlWatch, series(html, reload));
+	// src(jsURL + 'app.min.js')
+	// 	.pipe( notify({ message: 'Gulp is Watching, Happy Coding!' }) );
 }
 
 task("css", css);
 task("js", js);
 task("images", images);
 task("fonts", fonts);
-task("html", html);
-task("default", parallel(css, js, images, fonts, html));
+task("icons", icons);
+// task("html", html);
+task("default", parallel(css, js, images, fonts, icons));
 task("watch", parallel(browser_sync, watch_files));
