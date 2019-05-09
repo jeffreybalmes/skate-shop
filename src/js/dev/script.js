dba725dd63ae59a44bf99fc67905035535eb336b
jeffreybalmes <31235599+jeffreybalmes@users.noreply.github.com>
2019-05-09 12:57:25 +0800

    re organize src and dist directory

--- src/js/dev/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".burger-nav").addEventListener("click", function(){
-	var burger = document.querySelector("ul#primary-menu");
-    burger.classList.toggle("open");
-});
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger-nav").addEventListener("click", function(){
+   var burger = document.querySelector("ul#primary-menu");
+   burger.classList.toggle("open");
+});
